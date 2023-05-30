526ff1af4752aae044df889d5fb9cb9773d8ce44
Andres Rangel <rangelandres323@gmail.com>
2023-05-30 13:06:24 -0500

    formulario consulta general

--- src/app/home/report/report.component.ts
+++ src/app/home/report/report.component.ts
@@ -1,15 +1,89 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+import { Menssage } from 'src/app/models/router';
+import { AlertService } from 'src/app/service/alert.service';
+import { AuthService } from 'src/app/service/auth.service';
+import { LocalstoreService } from 'src/app/service/localstore.service';
+export interface userDataList{
+  id: string,
+  names: string,
+  lastnames: string,
+  cc: string,
+  email: string,
+  phone: string,
+  category: string
+}
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent implements OnInit {
-
-  constructor() { }
+  public customerDetail: any = [];
+  public selectItems: any;
+  public eventsItems: any = [];
+  public usersData: any;
+  public userCategory: any = [];
+  public form: FormGroup;
+  public dataSource: MatTableDataSource<userDataList>
+  displayedColumns: string[] = ['names', 'lastnames', 'cc', 'phone', 'productInterest','email', 'category'];
+  public eventList: userDataList[] = []
+  constructor(
+    private localStorage: LocalstoreService,
+    private myFormBuilder: FormBuilder,
+    private _https: AuthService,
+    private alert: AlertService,
+  ) { 
+    this.customerDetail = this.localStorage.getItem(Menssage.customerDetail);
+    this.usersData = this.localStorage.getSuccessLogin();
+    this.getEvents(this.usersData.user.idClientsProjects);
+    this.getCategories()
+  }
 
   ngOnInit(): void {
+    this.initial()
+  }
+  initial(){
+    this.form = this.myFormBuilder.group({
+      category: [Menssage.empty, Validators.compose([Validators.nullValidator])],
+      events: [Menssage.empty, Validators.compose([Validators.nullValidator])],
+      date: [Menssage.empty, Validators.compose([Validators.nullValidator])],
+    })
+    this.loadData()
+  } 
+  saveData(){
+    console.log(this.form)
+  }
+  getEvents(item: number) {
+    this.alert.loading();
+    this._https.getEvent(item).then((resulta: any) => {
+      console.log(resulta);
+      this.eventsItems = resulta
+      this.alert.messagefin();
+    }).catch((err: any) => {
+      console.log(err)
+      this.alert.error(Menssage.error, Menssage.server);
+    });
+  }
+  //INTENTA TRAER CATEGORIAS
+  getCategories(){
+    this.alert.loading();
+    this._https.getCategory().then((resulta:any)=>{
+      console.log(resulta);
+      this.userCategory=resulta
+      this.alert.messagefin();
+    }).catch((err: any) => {
+      console.log(err)
+      this.alert.error(Menssage.error, Menssage.server);
+    });
+  }
+  loadData() {
+    this.dataSource = new MatTableDataSource(this.eventList)
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
--- src/app/home/report/report.module.ts
+++ src/app/home/report/report.module.ts
@@ -3,6 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { ReportRoutingModule } from './report-routing.module';
 import { ReportComponent } from './report.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -11,7 +21,18 @@ import { ReportComponent } from './report.component';
   ],
   imports: [
     CommonModule,
-    ReportRoutingModule
+    ReportRoutingModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatRadioModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
   ]
 })
 export class ReportModule { }
